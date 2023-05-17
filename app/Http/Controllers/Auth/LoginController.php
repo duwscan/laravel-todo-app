b51f7b3d03d9ec7b07caab68c1a554a4fa46b721
duwscan <109639466+DuwscAn@users.noreply.github.com>
2023-05-18 02:53:12 +0700

    init project
    implenmant auth with sanctum and add signin

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
+
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(LoginRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['the credentials you entered ara incorrect.']
+            ]);
+        }
+        return response()->json(['message' => 'login success', 'user' => $user], ResponseAlias::HTTP_OK);
+    }
+}

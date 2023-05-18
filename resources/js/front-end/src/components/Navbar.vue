00e25a339e9f91358faf8685b4b1f6ff06339892
duwscan <109639466+DuwscAn@users.noreply.github.com>
2023-05-18 19:40:50 +0700

    implement vue

---
+++ resources/js/front-end/src/components/Navbar.vue
@@ -0,0 +1,72 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+        <div class="container py-2">
+            <a href="#" class="navbar-brand">
+                <span>Toedoe</span>
+                <strong>List</strong>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link">Tasks</a>
+                        <!-- <a href="#" @click.prevent="$router.push('/tasks')" class="nav-link">Tasks</a> -->
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">Summary</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ms-auto">
+                    <template v-if="true">
+                        <li class="nav-item">
+                            <a class="btn btn-outline-secondary ms-2">Login
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="btn btn-danger ms-2">Register</a>
+                        </li>
+                    </template>
+                    <template v-else>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" :class="toggleClass" @click.prevent="toggle" href="#"
+                               role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                User name
+                            </a>
+                            <ul class="dropdown-menu" :class="toggleClass">
+                                <li><a href="#" class="dropdown-item">Logout</a></li>
+                            </ul>
+                        </li>
+                    </template>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+import {ref, computed} from "vue";
+// import {useRouter} from "vue-router";
+// import {useAuthStore} from "../stores/auth";
+//
+// const router = useRouter()
+// const store = useAuthStore()
+const isOpen = ref(false)
+//
+// const logout = async () => {
+//     await store.handleLogout()
+//     isOpen.value = false
+//     router.push({name: 'login'})
+// }
+
+const toggle = () => isOpen.value = !isOpen.value
+
+const toggleClass = computed(() => isOpen.value === true ? 'show' : '')
+</script>
+<style scoped>
+.nav-link.div-active {
+    color: rgba(0, 0, 0, .9);
+}
+</style>
